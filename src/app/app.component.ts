728cc6d8c63d89992620b08175e569607aafe051
unknown <bouzire.noureddine1@gmail.com>
2025-01-23 15:37:16 +0100

    adding components and models to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { DUMMY_USERS } from './dummy-users';
+import { User } from './user/user.model';
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+ selector: 'app-root',
+ standalone: true,
+ imports: [HeaderComponent, UserComponent, TasksComponent], 
+ templateUrl: './app.component.html',
+ styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'EtrepriseTasks';
+ users = DUMMY_USERS; 
+
+
+ selectedUserId?: string;
+
+ get selectedUser(){
+   return this.users.find(user => user.id === this.selectedUserId)!;
+ }
+ onSelectUser(id: string) { 
+     this.selectedUserId = id;
 }
+}
+
---
+++ src/app/user/user.model.ts
@@ -0,0 +1,12 @@
+export interface User{
+    id: string;
+    name: string;
+    avatar: string; 
+  }
+   
+  
+  export interface NewTaskData{
+      title: string;
+      summary: string;
+      dueDate: string;
+  }
